7429ca7b0b723afb516651280115c19b0337c574
Moritz Fuller <moritz.fuller@dfinity.org>
2024-06-03 13:12:56 +0200

    ✨ add inter canister call to cketh ledger

--- src/logs_example_backend/src/lib.rs
+++ src/logs_example_backend/src/lib.rs
@@ -1,5 +1,6 @@
 use std::cell::RefCell;
 
+use candid::{Nat, Principal};
 use ic_cdk::{
     api::management_canister::ecdsa::{
         sign_with_ecdsa, EcdsaCurve, EcdsaKeyId, SignWithEcdsaArgument,
@@ -28,6 +29,25 @@ async fn sign() -> String {
         Ok(_) => println!("management canister call succeeded"),
         Err(err) => println!("management canister call failed: {:?}", err),
     }
+    "this returned".to_string()
+}
+
+#[ic_cdk::update]
+async fn icrc() -> String {
+    println!("pre cketh ledger call");
+
+    match ic_cdk::call::<(), (Nat,)>(
+        Principal::from_text("ss2fx-dyaaa-aaaar-qacoq-cai")
+            .expect("Could not decode the principal."),
+        "icrc1_total_supply",
+        (),
+    )
+    .await
+    {
+        Ok(total_supply) => println!("total supply {}", total_supply.0),
+        Err(err) => println!("ledger call failed: {:?}", err),
+    }
+
     "this returned".to_string()
 }
 
